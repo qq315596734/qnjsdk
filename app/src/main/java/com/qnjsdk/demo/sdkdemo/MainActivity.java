6a47be8e1252f845df99bc34c6713f034aa04229
zhaoweibo <zhaoweibo@xianjincard.com>
2019-07-16 17:48:27 +0800

    v1.1.0 版本完成

--- app/src/main/java/com/qnjsdk/demo/sdkdemo/MainActivity.java
+++ app/src/main/java/com/qnjsdk/demo/sdkdemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.qckj.qnjsdk.QNJSdk;
+import com.qckj.qnjsdk.QnjsdkBackCallback;
 import com.qckj.qnjsdk.QnjsdkCallback;
 import com.qckj.qnjsdk.bean.QnjsdkResult;
 import com.qnjsdk.demo.R;
@@ -17,6 +18,7 @@ public class MainActivity extends Activity {
 
     EditText et_uuid;
     EditText et_phone;
+    EditText et_color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +27,33 @@ public class MainActivity extends Activity {
 
         et_uuid = findViewById(R.id.et_uuid);
         et_phone = findViewById(R.id.et_phone);
+        et_color = findViewById(R.id.et_color);
+
 
     }
 
     public void clickStart(View view) {
         String uuid = et_uuid.getText().toString();
         String phone = et_phone.getText().toString();
+        String color = et_color.getText().toString();
+        //设置主题色
+        QNJSdk.setThemeColor(color);
+        //设置退出sdk的回调方法
+        QNJSdk.setBackCallback(new QnjsdkBackCallback() {
+            @Override
+            public void onBack() {
+                Toast.makeText(MainActivity.this, "退出了sdk", Toast.LENGTH_LONG).show();
+            }
+        });
         /*
             Context context           上下文
             String phone              用户手机号
             String uid                用户uid
             QnjsdkCallback callback   sdk启动成功失败的回调
          */
-        Log.e("111111111111", "uuid :" + uuid + "phone :" + phone);
         QNJSdk.start(MainActivity.this, phone, uuid, new QnjsdkCallback() {
             @Override
             public void onResult(QnjsdkResult result) {
-                Log.e("111111111111", "code :" + result.getCode() + "msg: " + result.getMsg());
                 Toast.makeText(MainActivity.this, "code :" + result.getCode() + "\nmsg: " + result.getMsg(), Toast.LENGTH_LONG).show();
             }
         });
